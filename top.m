f4ce73a3eca951e10d9d3704cead51debdc255e8
Alvaro Suarez Menendez <161531596+AlvaroSM19@users.noreply.github.com>
2024-05-10 12:07:52 +0200

    Update top.m
    Now, sat_ids go from 0 to 71.
    Previusly it was from 1 to 72

--- top.m
+++ top.m
@@ -63,7 +63,7 @@ for i = 1 : sim_time
     msg = string_gen(tow,week,satellites_t(sat_n,:),l);     %Generate bitstream for an specific satellite
     msg_mod = psk(msg,cardinality,fs,tbit,power);  %Modulates de bitstream
     msg_mac = fdma(msg_mod,df,fs,satellites_t{sat_n,1},nsat); % Applies MAC to the modulated samples of an specific satellite
-    full_msg(satellites_t{sat_n,1},fs*(i-1)+1:fs*i) = msg_mac;   %Includes de samples on the main matrix
+    full_msg(satellites_t{sat_n,1}+1,fs*(i-1)+1:fs*i) = msg_mac;   %Includes de samples on the main matrix
     end
     date = update_time(date,1);     %Increments the date by 1 second
 end
@@ -71,6 +71,6 @@ end
 % For loop - Writes the data from the main matrix to .bin file.
 %One file for each satellite and one file for each I-Q component
 for sat_n = 1:nsat
-    disp("Writing data of satellite " + (sat_n))    %Indicator for runtime
-    write_file(full_msg(sat_n,1:end),sat_n);        %Function to write I-Q samples of an specific satellite
+    disp("Writing data of satellite " + (sat_n-1))    %Indicator for runtime
+    write_file(full_msg(sat_n,1:end),sat_n-1);        %Function to write I-Q samples of an specific satellite
 end
